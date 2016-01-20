04410edde8660fa3fe3bbe97b35abaf7cb9ecfac
Abby Milberg <abby805@gmail.com>
2016-01-20 14:12:44 -0500

    Adding autoprefixer and cssnano to package.json.

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,8 @@ module.exports = function (grunt) {
       options: {
         map: true,
         processors: [
-          require('autoprefixer')({browsers: ['last 2 versions', 'ie >= 10']})
+          require('autoprefixer')({browsers: ['last 2 versions', 'ie >= 10']}),
+          require('cssnano')()
         ]
       },
       dev: {
