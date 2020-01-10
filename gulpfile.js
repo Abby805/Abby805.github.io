c894da94f57d2a60e1a45b49bf139b347b8257c1
Abby Milberg <abby805@gmail.com>
2020-01-10 00:14:21 -0500

    Updating to modern method for declaring browsers for use with autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -43,11 +43,7 @@ gulp.task('scss', () => {
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(cssnano({zindex: false}))
-    .pipe(postcss([
-      prefix({
-        browsers: ['last 3 versions'],
-        cascade: false })
-      ]))
+    .pipe(postcss([prefix({cascade: false})]))
     .pipe(sourcemaps.write('maps'))
     .pipe(gulp.dest(paths.styles.dist));
 });
